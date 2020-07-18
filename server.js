1c9a8faccab69cc0241d2512e632e1f9109f2e1a
Gianluca Maccioni <gianluca.maccioni.uk@gmail.com>
2020-07-18 17:36:13 +0100

    Ready for local dev

--- server.js
+++ server.js
@@ -11,13 +11,14 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.get('/hello', function (req, res) {
+  res.json({ greetings: "Hello, API" });
 });
+
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
