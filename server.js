a84b101bbe3b3d14a4ecb551d6fd241739d5e04c
Gianluca Maccioni <gianluca.maccioni.uk@gmail.com>
2020-07-19 07:03:58 +0100

    Implement POST /apy/fileanalyse

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+const multer = require('multer')
+const storage = multer.memoryStorage()
+const upload = multer({
+  storage,
+  limits: { fileSize: 50 * 1024 * 1024 }
+}).single('upfile')
 
 var app = express();
 
@@ -18,6 +24,30 @@ app.get('/hello', function (req, res) {
   res.json({ greetings: "Hello, API" });
 });
 
+app.post('/api/fileanalyse', (req, res) => {
+  upload(req, res, err => {
+    if (err) {
+      console.log('err', err)
+      res.status(500).json({ error: 'internal error' })
+    } else {
+      if (req.file === undefined) {
+        res.status(400).json({ error: 'no file selected' })
+      } else {
+        // Using memory storage. Free memory immediately!
+        req.file.buffer = null;
+
+        const f = req.file
+        console.log('file metadata:\n', f)
+        res.json({
+          name: f.originalname,
+          type: f.mimetype,
+          size: f.size
+        })
+      }
+    }
+  })
+
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
